b52e42d265cb17d8ff77f7a1ee5acd7f690341fb
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2022-09-21 07:38:28 -0300

    ForEach recap

---
+++ src/main/java/dev/brunoliveira/arrays/ForEach.java
@@ -0,0 +1,12 @@
+package dev.brunoliveira.arrays;
+
+public class ForEach {
+
+    public static void main(String[] args) {
+        double[] notas = {9.9, 10, 8.7, 7.2, 6.5, 5.6, 4.6, 3.6, 9.4};
+
+        for(double nota : notas) {
+            System.out.println(nota);
+        }
+    }
+}
