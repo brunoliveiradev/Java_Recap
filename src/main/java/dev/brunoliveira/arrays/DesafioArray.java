e52bc09db10e3ab01624a6c8dcb242294bc9cfff
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2022-09-21 08:26:32 -0300

    Challenged de array resolvido.

---
+++ src/main/java/dev/brunoliveira/arrays/DesafioArray.java
@@ -0,0 +1,30 @@
+package dev.brunoliveira.arrays;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DesafioArray {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de alunos que será calculado a média de notas: ");
+        int sizeArray = reader.nextInt();
+        double[] notas = new double[sizeArray];
+
+        System.out.printf("Digite as %s notas de alunos a serem calculadas média: %n", sizeArray);
+
+        for (int i = 0; i < sizeArray; i++) {
+            notas[i] = reader.useLocale(Locale.ROOT).nextDouble();
+        }
+        reader.close();
+
+        double soma = 0;
+        for(double nota : notas) {
+            soma += nota;
+        }
+
+        double media = (soma / sizeArray);
+        System.out.printf("A média de notas é: %.2f", media);
+    }
+}
