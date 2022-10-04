478e8430f38d6443c30d4aa19a5b6630b86f9ad1
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2022-10-03 22:05:36 -0300

    Recap sobre SortedSet e TreeSet com dados heterogeneos.

---
+++ src/main/java/dev/brunoliveira/collecoes/sets/ConjuntoHomogeneo.java
@@ -0,0 +1,35 @@
+package dev.brunoliveira.collecoes.sets;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoHomogeneo {
+
+    public static void main(String[] args) {
+
+        SortedSet<String> lista = new TreeSet<>();
+
+        lista.add("TreeSet faz parte tbm do sortedSet, que aceita criterio de ordenaçao, diferente do HashSet que não aceita.");
+        lista.add("Ana");
+        lista.add("Bruno");
+        lista.add("Henrique");
+
+        for(String candidato: lista) {
+            System.out.println(candidato);
+        }
+
+        Set<Double> numeros = new HashSet<>();
+        numeros.add(1.0);
+        numeros.add(2.0);
+        numeros.add(123.0);
+        numeros.add(2003.0);
+        numeros.add(5.0);
+        numeros.add(15.0);
+
+        for(Double n: numeros) {
+            System.out.println(n);
+        }
+    }
+}
