9e1bec47c8bf30dbd1e35dc5d1aa51efdb8622e8
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2022-11-15 19:29:09 -0300

    [UPDATE]: Recap sobre numeric auto conversion

---
+++ src/main/java/dev/brunoliveira/uuid/WaterTank.java
@@ -0,0 +1,30 @@
+package dev.brunoliveira.uuid;
+
+public class WaterTank {
+    private String brand;
+    private boolean empty;
+
+    public static void main(String[] args) {
+        WaterTank wb = new WaterTank();
+        System.out.println("Empty: " + wb.empty);
+        System.out.println(" brand: " + wb.brand);
+
+        double xDouble = 44.25;
+        float yFloat = 3.35F;
+        double zDouble = xDouble + yFloat;
+        System.out.println("double e float vira double: " + zDouble);
+
+        short xShort = 14;
+        short yShort = 6;
+        int zInt = xShort / yShort;
+        System.out.println("Operações matematicas entre shorts viram int: " + zInt);
+
+        short x = 14;
+        float y = 13f;
+        double z = 30;
+        double sum = x + y + z;
+        System.out.println("Operações matematicas entre short, float e double viram double: " + sum);
+
+
+    }
+}
